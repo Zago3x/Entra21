8c20835677c3187bef24ec239c1c9e9f8d5aa03d
LEANDRO ZAGONEL <leandro_zagonel@estudante.sc.senai.br>
2023-09-19 22:06:16 -0300

    20230919 - Aula Exercicio

---
+++ MeuprimeirohtmlJS/somanumeros.js
@@ -0,0 +1,14 @@
+
+
+let elementoInputUm = document.getElementById("inptUm")
+let elementoInputDois =document.getElementById("inptDois")
+let soma
+
+
+function SomaNumeros(){
+    soma = Number(elementoInputUm.value) + Number(elementoInputDois.value)
+    alert('Soma dos números ${soma}')
+
+
+
+}
